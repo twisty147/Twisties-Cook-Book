a86ac7d48cdd3956948d3fc3a592c5ddda1c12c8
Oliver Twisty <oamah1@yahoo.com>
2024-09-24 09:29:52 +0100

    Enhance recipe management: Add modals for delete confirmation and improve navigation

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,11 @@
+function goBack() {
+    window.history.back();
+}
 document.addEventListener('DOMContentLoaded', function () {
-     // Initialize Materialize
-     M.AutoInit();
+    // Initialize Materialize
+    M.AutoInit();
+
+
     // Reset filter functionality
     var resetFilter = document.getElementById('reset-filter');
     var urls = document.getElementById('urls');
@@ -20,14 +25,32 @@ document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
 
-   
+
 
     // Auto-hide flash messages after 5 seconds
-        setTimeout(function () {
+    setTimeout(function () {
         let message = document.getElementById('flashMessage');
         if (message) {
             message.style.display = 'none';
         }
     }, 5000); // 5000 milliseconds = 5 seconds
+
     
+
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+
+    // When a delete link is clicked
+    document.querySelectorAll('.modal-trigger').forEach(function (element) {
+        element.addEventListener('click', function () {
+            var recipeId = this.getAttribute('data-recipe-id');
+            var recipeTitle = this.getAttribute('data-recipe-title');
+            // Set the recipe title in the modal
+            document.getElementById('recipeTitle').textContent = recipeTitle;
+            // Set the delete form action dynamically
+            var deleteForm = document.getElementById('deleteForm');
+            deleteForm.setAttribute('action', '/delete_recipe/' + recipeId);
+        });
+    });
+
 });
