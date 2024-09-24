a8c6253d0e318d99491d200ba3c2f406222cd89d
Oliver Twisty <oamah1@yahoo.com>
2024-09-24 10:02:27 +0100

    Changing debug to True to continue development

--- app.py
+++ app.py
@@ -415,5 +415,5 @@ def delete_recipe(recipe_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-             debug=False)
+             debug=True)
     
